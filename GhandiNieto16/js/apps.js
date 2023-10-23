f39dda131c4c583d80c5d6903c8f510678ef294a
Ghandi Yamileth Nieto Proa <yamileth.nietoro@uanl.edu.mx>
2023-10-23 08:32:48 -0600

    clase 23-10-2023

--- GhandiNieto16/js/apps.js
+++ GhandiNieto16/js/apps.js
@@ -1,3 +1,92 @@
+/* App 4 */
+
+const app4Tabla = document.getElementById("app4Tabla");
+const app4Inv = document.getElementById("app4Inv");
+const app4SoloPunto = document.getElementById("app4SoloPunto")
+const app4Res = document.getElementById("app4Res");
+const app4BtnVer = document.getElementById("app4BtnVer");
+
+
+/* Checar si se marco la tabla invertida */
+
+if (app4Inv.checked){
+    
+};
+
+/* Inicialización, Repetición (mientras cont sea menor o igual a 10 el ciclo se va a estar reptitiendo), en cuanto aumenta */
+for(let cont = 1; cont <=10; cont++){
+
+    console.log(cont);
+
+}
+
+
+
+/* App 3 */
+
+
+const app3Num1 = document.getElementById("app3Num1");
+const app3Num2 = document.getElementById("app3Num2");
+const app3Operacion = document.getElementById("app3Operacion");
+const app3Res = document.getElementById("app3Res");
+const app3BtnCalcular = document.getElementById("app3BtnCalcular");
+app3BtnCalcular.addEventListener("click", (e) => {
+
+    /* Esto es para que no se envien los datos al servidor */
+    e.preventDefault();
+    /* Variable que guarda el valor que esta en el objeto app3Num1 */
+    let num1 = parseInt(app3Num1.value);
+    let num2 = parseInt(app3Num2.value);
+    /* Esto se devuelve como cadena, por ejemplo si el usuario elije la resta, el value ontendra la letra r */
+    let operacion = app3Operacion.value;
+
+/*     if (operacion == "s"){
+
+        resultado = num1 + num2 ;
+
+    }
+
+    else if(operacion == "r") {
+        resultado = num1 - num2;
+    }
+
+    else if(operacion == "m") {
+        resultado = num1 * num2;
+    }
+
+    else if(operacion == "d") {
+        resultado = num1 / num2;
+    } */
+
+    switch(operacion){
+        case "s": resultado = num1 + num2;
+                    break;
+
+        case "r": resultado = num1 - num2; break;
+
+        case "m": resultado = num1 * num2; break;
+        case "d": resultado = num1 / num2; break;
+
+    }
+
+
+    app3Res.value = resultado;
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* App1 */
 
 
