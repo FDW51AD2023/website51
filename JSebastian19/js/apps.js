61d446749bb3c1a36d1d3a1a2ffcdaa5712779f7
Jose Sebastian Valdez Navarro <JsebastianVal19@gmail.com>
2023-10-23 21:24:29 -0600

    Operaciones

--- JSebastian19/js/apps.js
+++ JSebastian19/js/apps.js
@@ -1,18 +1,29 @@
-/* App1 * */
+/* Aplicacion 1 */
 
-const app1Num1 = document.getElementById("app1Num1");
-const app1Num2 = document.getElementById("app1Num2");
+const app1Num1 = document.getElementById ("app1Num1");
+const app1Num2 = document.getElementById ("app1Num2");
 
-const app1Res = document.getElementById("app1Res");
+const app1BotonCalcular = document.getElementById ("app1BtnCalcular");
 
-const app1BotonCalcular = document.getElementById("app1BtnCalcular");
+const app1ResSuma = document.getElementById ("app1ResSuma");
+const app1ResResta = document.getElementById ("app1ResResta");
+const app1ResMultiplicacion = document.getElementById ("app1ResMultiplicacion");
+const app1ResDivsion = document.getElementById ("app1ResDivision");
 
 app1BotonCalcular.addEventListener("click", (e) => {
     e.preventDefault();
 
-    let num1 = parseInt (app1Num1.value)
-    let num2 = parseInt (app1Num2.value)
-    let suma = num1 + num2
+    let num1 = parseFloat (app1Num1.value);
+    let num2 = parseFloat (app1Num2.value);
 
-    app1Res.value = suma
+    let suma = num1 + num2;
+    let resta = num1 - num2;
+    let multiplicacion = num1 * num2;
+    let division = num1 / num2;
+
+    app1ResSuma.value = suma
+    app1ResResta.value = resta
+    app1ResMultiplicacion.value = multiplicacion
+    app1ResDivsion.value = division
+
 });
