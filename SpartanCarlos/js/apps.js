57cce5e7c5a0f5c7084976a12154c6bbfa28c48a
luis carlos gutierrez cerda <luiscarlosgtz45@gmail.com>
2023-10-30 08:30:41 -0600

    Clase del 30 Octubre Finalizada

--- SpartanCarlos/js/apps.js
+++ SpartanCarlos/js/apps.js
@@ -165,3 +165,42 @@ app6SoloPunto.addEventListener("change" (e) => {
 /* App7 */
 
 
+/* App8 */
+function perfecto(numero){
+    let div = 1;
+    let suma= 0;
+    while (div < numero){
+        if(numero % div == 0){
+        suma += div 
+        }
+         div++;
+    }
+    return suma == numero;
+}
+
+const app8Ini = document.getElementById("app8Ini");
+const app8Fin = document.getElementById("app8Fin");
+const app8Res = document.getElementById("app8Res");
+const app8BtnVer =document.getElementById("app8BtnVer");
+const lista = document.createElement("ol");
+
+app8BtnVer.addEventListener("click",(e) => {
+    e.preventDefault();
+    let ini = parseInt(app8Ini.value);
+    let fin = parseInt(app8Fin.value);
+    
+    let numero = ini
+
+    do {  
+        /* Codigo para checar si es perfecto */
+        if(perfecto(numero)){
+            let item = document.createElement("li");
+            item.innerHTML = numero;
+            lista.appendChild(item);
+        }
+        /* Codigo para checar ese numero */
+        numero++;
+    } while(numero <= fin);
+
+   app8Res.appendChild(lista);
+})
