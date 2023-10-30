07404390de5bd4a74f9cad46ce9cc38a738b5f01
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2023-10-30 07:22:53 -0600

    clase 15 oct

--- liigabriel/js/apps.js
+++ liigabriel/js/apps.js
@@ -5,10 +5,46 @@ const app4SoloPunto = document.getElementById("app4SoloPunto");
 const app4Res = document.getElementById("app4Res");
 const app4BtnVer = document.getElementById("app4BtnVer");
 
-/* checar si se marco tabla invertida */
-if ( app4Inv.checked ){
+function mostrarTabla(){
+    let tabla =  parseInt(app4Tabla.value);
 
+    let lista = document.createElement("ul");
+    
+    if( !app4Inv.checked ){
+        for(let i=1 ; i <=10; i++ ){
+            let resMult = app4SoloPunto.checked ? ".".repeat(tabla * i) : tabla * i ;
+
+            let item = document.createElement("li");
+            item.innerHTML = tabla + " X " + i + " = " + resMult;
+            lista.appendChild(item);
+        }
+    }
+    else {
+        for(let i=10 ; i >=1; i-- ){
+
+            let resMult = app4SoloPunto.checked ? ".".repeat(tabla *i): tabla*i;
+
+            let item = document.createElement("li");
+            item.innerHTML = tabla + " X " + i + " = " + resMult;
+            lista.appendChild(item);
+        }
+    }
+    app4Res.innerHTML = "";
+    app4Res.appendChild(lista);
 }
+
+app4BtnVer.addEventListener("click", (e) => {
+    e.preventDefault();
+    mostrarTabla();
+});
+
+app4Inv.addEventListener("change", (e) => {
+    mostrarTabla();
+});
+
+app4SoloPunto.addEventListener("change", (e) => {
+    mostrarTabla();
+});
 
 
 
