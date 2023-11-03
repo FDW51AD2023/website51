f67e3f94bc95c3beab3bac5d83376d84b21c300d
Angel Alejandro Galaviz Rivera <angel.galavizrvr@uanl.edu.mx>
2023-11-02 19:03:04 -0600

    cambios en mi repo

--- angel-alejandro-galaviz-rivera/js/apps.js
+++ angel-alejandro-galaviz-rivera/js/apps.js
@@ -194,49 +194,44 @@ app5BtnCalcular.addEventListener ("click" , (e) => {
 
 /*App7*/
 function perfecto(numero){
-    let div =1;
-    let suma =0;
-    
+    let div = 1;
+    let suma = 0;
     while(div < numero){
         if(numero % div == 0){
-            suma+=div
+            suma += div;
         }
         div++;
     }
     return suma == numero;
 }
 
-
-const app7Init = document.getElementById("app7Init");
+const app7Ini = document.getElementById("app7Ini");
 const app7Fin = document.getElementById("app7Fin");
 const app7Res = document.getElementById("app7Res");
 const app7BtnVer = document.getElementById("app7BtnVer");
-
 const lista = document.createElement("ol");
 
-app7BtnVer.addEventListener("click" , (e) => {
-    e.preventDefault;
 
-    let ini = parseInt(app7Init.value);
-    let fin =parseInt(app7Fin.value);
+app7BtnVer.addEventListener("click", (e) => {
+    e.preventDefault();
+    let ini = parseInt(app7Ini.value);
+    let fin = parseInt(app7Fin.value);
+
     let numero = ini;
 
-    /*Codigo para checar ese numero*/
-
     do {
-        /*Codigo para checar si el numero es perfecto*/
-        if(perfecto(numero)){
+        /* codigo para checar si es perfecto*/
+        if( perfecto(numero)){
             let item = document.createElement("li");
             item.innerHTML = numero;
             lista.appendChild(item);
-
         }
         numero++;
-    }while(ini <= fin);
-
+    }while(numero <= fin);
     app7Res.appendChild(lista);
-
 });
+
+
 /*App7*/
 
 /*App8*/
